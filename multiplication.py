61cd1db0b09eafcfd5b98f61dc40259ca12c3229
Dominique <dominique.fadeur@unamur.be>
2019-07-11 16:06:00 +0200

    oki

---
+++ multiplication.py
@@ -0,0 +1,7 @@
+def multiplication(nb, limite):
+
+    i = 0  # C'est notre variable compteur que nous allons incrémenter dans la boucle
+
+    while i < limite:  # Tant que i est strictement inférieure à 10
+        print(i + 1, "*", nb, "=", (i + 1) * nb)
+        i += 1  # On incrémente i de 1 à chaque tour de boucle
---
+++ multiply.py
@@ -0,0 +1,18 @@
+from multiplication import *
+
+nombre = input("nombre : ")
+
+try:
+    nombre = int(nombre)
+except:
+    print("vous n'avez pas entré un nombre correct")
+else:
+    limite = input("limite : ")
+    try:
+        limite = int(limite)
+    except:
+        print("vous n'avez pas entré une limite correcte")
+    else:
+        multiplication(nombre, limite)
+
+
